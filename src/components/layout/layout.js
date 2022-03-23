412ddcb624f01a750d96c113cada68fce847e4e8
AxelUrizar <axelurizarmartin@gmail.com>
2022-03-23 22:39:48 +0000

    components folder created

---
+++ src/components/layout/layout.js
@@ -0,0 +1,10 @@
+export const Layout = () => {
+    const players = 4
+
+    return (
+        <div>
+            <h2>Bienvenido!</h2>
+            <p>Ahora mismo somos {players} jugadores!</p>
+        </div>
+    )
+}
