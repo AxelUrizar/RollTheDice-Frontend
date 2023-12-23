bc7dca14221c8ce2fcda3c52b1de451285c19152
AxelUrizar <axelurizarmartin@gmail.com>
2023-12-23 20:05:31 +0100

    changed main route

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='' element={<Layout />}>
+        <Route path='/RollTheDice-Frontend' element={<Layout />}>
           <Route path='' element={<Home />} />
 
           {/* Session endpoints */}
